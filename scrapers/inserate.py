4ca518957ced4b9273cb08e7a1454032faa3c74f
Daniel Stoiber <dastoiboy@gmail.com>
2025-09-02 23:56:42 +0200

    Added distance, preview imgs, and strict search
    - Added distance (in km) to /inserate items
    - Added preview image url to /inserate items
    - Added strict_search boolean query param to /inserate to ignore alternative results

--- scrapers/inserate.py
+++ scrapers/inserate.py
@@ -1,3 +1,5 @@
+import re
+
 from urllib.parse import urlencode
 
 from fastapi import HTTPException
@@ -11,7 +13,8 @@ async def get_inserate_klaz(browser_manager: PlaywrightManager,
                             radius: int = None,
                             min_price: int = None,
                             max_price: int = None,
-                            page_count: int = 1):
+                            page_count: int = 1,
+                            strict_search: bool = False):
     base_url = "https://www.kleinanzeigen.de"
 
     # Build the price filter part of the path
@@ -44,8 +47,12 @@ async def get_inserate_klaz(browser_manager: PlaywrightManager,
         results = []
 
         for i in range(page_count):
-            page_results = await get_ads(page)
-            results.extend(page_results)
+
+            # Check if the search query yields no exact results
+            no_results_banner = await page.query_selector("#saved-search-empty-result")
+            if not (no_results_banner and strict_search):
+                page_results = await get_ads(page)
+                results.extend(page_results)
 
             if i < page_count - 1:
                 try:
@@ -62,6 +69,7 @@ async def get_inserate_klaz(browser_manager: PlaywrightManager,
 
 
 async def get_ads(page):
+
     try:
         items = await page.query_selector_all(".ad-listitem:not(.is-topad):not(.badge-hint-pro-small-srp)")
         results = []
@@ -70,6 +78,28 @@ async def get_ads(page):
             if article:
                 data_adid = await article.get_attribute("data-adid")
                 data_href = await article.get_attribute("data-href")
+
+                # Distance (if available)
+                distance_element = await article.query_selector("div.aditem-main--top > div.aditem-main--top--left")
+                distance_text = await distance_element.inner_text() if distance_element else ""
+                distance_text = distance_text.strip().replace("\n", " ")
+                # Apply regex to extract distance
+                if "km" in distance_text.lower():
+                    matches = re.findall(r"\((?:[^)]*?)(\d+(?:[.,]\d+)?)\s*km\)", distance_text, flags=re.IGNORECASE)
+                    if matches:
+                        value = matches[-1].replace(",", ".")  # take last match, normalize decimal comma
+                        distance = float(value)
+                        if distance.is_integer():
+                            distance = int(distance)
+                    else:
+                        distance = 0
+                else:
+                    distance = 0
+
+                # Preview image
+                preview_img_element = await article.query_selector(".aditem-image > a > div > img")
+                preview_img_url = await preview_img_element.get_attribute("src") if preview_img_element else ""
+
                 # Get title from h2 element
                 title_element = await article.query_selector("h2.text-module-begin a.ellipsis")
                 title_text = await title_element.inner_text() if title_element else ""
@@ -82,7 +112,7 @@ async def get_ads(page):
                 description_text = await description.inner_text() if description else ""
                 if data_adid and data_href:
                     data_href = f"https://www.kleinanzeigen.de{data_href}"
-                    results.append({"adid": data_adid, "url": data_href, "title": title_text, "price": price_text, "description": description_text})
+                    results.append({"adid": data_adid, "url": data_href, "preview_image_url": preview_img_url, "title": title_text, "price": price_text, "description": description_text, "distance": distance})
         return results
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
